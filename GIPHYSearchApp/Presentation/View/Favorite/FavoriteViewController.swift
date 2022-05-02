4de27e86aa98fde4d0e6ddf9eedf1a84364f1a32
EthanPark <ethanpark604@gmail.com>
2022-05-02 23:39:30 +0900

    [Edit] Gradient Config

---
+++ GIPHYSearchApp/Presentation/View/Favorite/FavoriteViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteViewController.swift
+//  GIPHYSearchApp
+//
+//  Created by 박근보 on 2022/05/02.
+//
+
+import Foundation
--- GIPHYSearchApp/Util/Extension/UIView+Extension.swift
+++ GIPHYSearchApp/Util/Extension/UIView+Extension.swift
@@ -8,15 +8,12 @@
 import UIKit
 
 extension UIView {
-    func setGradient() {
+    func setGradient(startColor: UIColor, finishColor: UIColor, start: CGPoint, end: CGPoint) {
         let gradient: CAGradientLayer = CAGradientLayer()
-        gradient.colors = [
-            UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 0).cgColor,
-            UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.3).cgColor
-        ]
+        gradient.colors = [startColor.cgColor, finishColor.cgColor]
         gradient.locations = [0.0 , 1.0]
-        gradient.startPoint = CGPoint(x: 1.0, y: 0.0)
-        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.startPoint = start
+        gradient.endPoint = end
         gradient.frame = bounds
         layer.addSublayer(gradient)
     }
