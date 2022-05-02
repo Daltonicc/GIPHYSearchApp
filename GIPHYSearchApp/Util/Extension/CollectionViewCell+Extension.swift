0789aff679b4d1d15b8ce46558081fc0acd91f83
EthanPark <ethanpark604@gmail.com>
2022-05-02 18:09:16 +0900

    [Add] CollectionView Extension

---
+++ GIPHYSearchApp/Util/Extension/CollectionViewCell+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell+Extension.swift
+//  GIPHYSearchApp
+//
+//  Created by 박근보 on 2022/05/02.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
