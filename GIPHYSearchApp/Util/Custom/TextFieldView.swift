471de996915e55a728e35934ce10ad6826c78fae
EthanPark <ethanpark604@gmail.com>
2022-05-02 19:22:02 +0900

    [Add] TextFieldView

--- GIPHYSearchApp/Util/Custom/TextFieldView.swift
+++ GIPHYSearchApp/Util/Custom/TextFieldView.swift
@@ -1,8 +1,57 @@
 //
-//  CustomTextField.swift
+//  TextFieldView.swift
 //  GIPHYSearchApp
 //
 //  Created by 박근보 on 2022/05/02.
 //
 
-import Foundation
+import UIKit
+
+final class TextFieldView: BaseView {
+
+    let textField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Search GIPHY"
+        textField.borderStyle = .none
+        textField.addLeftPadding()
+        return textField
+    }()
+    let searchButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemPurple
+        button.tintColor = .white
+        button.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func setUpView() {
+
+        self.backgroundColor = .white
+
+        addSubview(textField)
+        addSubview(searchButton)
+    }
+
+    override func setUpConstraint() {
+
+        textField.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        textField.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        textField.trailingAnchor.constraint(equalTo: searchButton.leadingAnchor).isActive = true
+        textField.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+
+        searchButton.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        searchButton.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        searchButton.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        searchButton.widthAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+}
