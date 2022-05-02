36673b08b65eb53f9b5f34b5cbe36b1e1a0cbe0c
EthanPark <ethanpark604@gmail.com>
2022-05-03 01:50:19 +0900

    [Del] Base NavigationController

--- GIPHYSearchApp/Application/SceneDelegate.swift
+++ GIPHYSearchApp/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
         window?.windowScene = windowScene
-        window?.rootViewController = UINavigationController(rootViewController: SearchViewController())
+        window?.rootViewController = HomeTabBarController()
         window?.makeKeyAndVisible()
     }
 
