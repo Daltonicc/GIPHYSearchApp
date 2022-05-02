054f35412eb6ad53e4c9a2eb439217875c85b33f
EthanPark <ethanpark604@gmail.com>
2022-05-03 01:47:06 +0900

    [Add] GIPHYRepository

--- GIPHYSearchApp/Data/Repository/GIPHYRepository.swift
+++ GIPHYSearchApp/Data/Repository/GIPHYRepository.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Alamofire
+
+final class GIPHYRepository {
+
+    func getGiphyData(query: String, start: Int, display: Int, completion: @escaping (Result<GIFData, SearchError>) -> Void) {
+
+        let giphyAPI: GIPHYAPI = .getGifData(query: query, start: start, display: display)
+
+        AF.request(giphyAPI.url, method: .get, parameters: giphyAPI.parameters, headers: giphyAPI.headers).validate()
+            .responseDecodable(of: GIFSearchResponseDTO.self) { response in
+                switch response.result {
+                case .success(let data):
+                    let gifData = data.toEntity()
+                    completion(.success(gifData))
+                case .failure(_):
+                    guard let statusCode = response.response?.statusCode else { return }
+                    completion(.failure(SearchError(rawValue: statusCode) ?? .badRequest))
+                }
+            }
+    }
+}
