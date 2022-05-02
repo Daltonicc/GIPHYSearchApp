9fdfa198abf7d61c24faf36eccaba5e56251f496
EthanPark <ethanpark604@gmail.com>
2022-05-03 01:46:02 +0900

    [Add] Entity(GIFData)

--- GIPHYSearchApp/Domain/Entity/GIFData.swift
+++ GIPHYSearchApp/Domain/Entity/GIFData.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+struct GIFData {
+    let item: [GIFItem]
+    let pagination: Pagination
+}
+
+struct GIFItem {
+    let type: String
+    let id: String
+    let webPageURL: String
+    let title: String
+    let images: GIFCategory
+    let user: UserData
+    let isFavorite: Bool
+}
+
+struct GIFCategory {
+    let original: GIFSize
+    let preview: GIFSize
+}
+
+struct GIFSize {
+    let height: String
+    let width: String
+    let size: String
+    let url: String
+}
+
+struct UserData {
+    let avatarURL: String
+    let name: String
+}
+
+struct Pagination {
+    let total: Int
+    let count: Int
+    let start: Int
+}
