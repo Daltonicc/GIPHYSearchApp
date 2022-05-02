3102b6835e98f1c2ba1d5f1d43581f71c6bd229d
EthanPark <ethanpark604@gmail.com>
2022-05-03 01:06:46 +0900

    [Add] Network Error

--- GIPHYSearchApp/Network/NetworkError.swift
+++ GIPHYSearchApp/Network/NetworkError.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum SearchError: Int, Error {
+    case badRequest = 400
+    case forbidden = 403
+    case notFoundData = 404
+    case tooLongQuery = 414
+    case tooManyRequest = 429
+}
+
+extension SearchError {
+    private var errorDescription: String {
+        switch self {
+        case .badRequest: return "잘못된 요청입니다"
+        case .forbidden: return "권한이 없습니다"
+        case .notFoundData: return "데이터를 찾을 수 없습니다"
+        case .tooLongQuery: return "50글자 이하로 작성해주세요"
+        case .tooManyRequest: return "잠시 후 다시 시도해주세요"
+        }
+    }
+}
