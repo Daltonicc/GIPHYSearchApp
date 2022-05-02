cf9233f3ecd5c5905da7c2942604cdd3e61b6ff8
EthanPark <ethanpark604@gmail.com>
2022-05-02 14:07:55 +0900

    [Add] Project Base Setting

--- GIPHYSearchApp/Presentation/View/Search/SearchViewController.swift
+++ GIPHYSearchApp/Presentation/View/Search/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  GIPHYSearchApp
 //
 //  Created by 박근보 on 2022/05/02.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
