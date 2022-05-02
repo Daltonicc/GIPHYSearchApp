0135a2c02802efa272d9ff241edf140716f19ce9
EthanPark <ethanpark604@gmail.com>
2022-05-03 04:53:01 +0900

    [Add] ImageCacheManager

--- GIPHYSearchApp/Util/Manager/ImageCacheManager.swift
+++ GIPHYSearchApp/Util/Manager/ImageCacheManager.swift
@@ -5,4 +5,11 @@
 //  Created by 박근보 on 2022/05/03.
 //
 
-import Foundation
+import UIKit
+
+final class ImageCacheManager {
+
+    static let shared = NSCache<NSString, UIImage>()
+
+    private init() {}
+}
