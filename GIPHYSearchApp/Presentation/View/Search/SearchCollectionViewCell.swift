3d74db9cf938e552b0181d76cd71a00dc18fd6e2
EthanPark <ethanpark604@gmail.com>
2022-05-03 21:53:04 +0900

    [Edit] Applying Custom ContentGIFView(SearchCollectionViewCell)

--- GIPHYSearchApp/Presentation/View/Search/SearchCollectionViewCell.swift
+++ GIPHYSearchApp/Presentation/View/Search/SearchCollectionViewCell.swift
@@ -9,20 +9,10 @@ import UIKit
 
 final class SearchCollectionViewCell: UICollectionViewCell {
 
-    let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.backgroundColor = .black
-        imageView.layer.masksToBounds = true
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-    let indicatorView: UIActivityIndicatorView = {
-        let indicatorView = UIActivityIndicatorView()
-        indicatorView.hidesWhenStopped = true
-        indicatorView.color = .white
-        indicatorView.backgroundColor = .black
-        indicatorView.translatesAutoresizingMaskIntoConstraints = false
-        return indicatorView
+    let cellView: ContentGIFView = {
+        let view = ContentGIFView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
 
     override init(frame: CGRect) {
@@ -36,40 +26,23 @@ final class SearchCollectionViewCell: UICollectionViewCell {
     }
 
     private func setUpCell() {
-        contentView.addSubview(imageView)
-        contentView.addSubview(indicatorView)
+        contentView.addSubview(cellView)
     }
 
     private func setUpConstraints() {
 
-        imageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-        imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-        imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-
-        indicatorView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        indicatorView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-        indicatorView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-        indicatorView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        cellView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        cellView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        cellView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        cellView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
     }
 
     func cellConfig(item: GIFItem) {
-        indiatorAction(bool: true)
+        cellView.indicatorAction(bool: true)
         let gifURL = item.images.preview.url
-        DispatchQueue.main.async {
-            self.imageView.image = UIImage.gifImageWithURL(gifURL)
-//            self.imageView.setImageUrl(gifURL)
-            self.indiatorAction(bool: false)
-        }
-    }
-
-    private func indiatorAction(bool: Bool) {
-        if bool {
-            indicatorView.isHidden = false
-            indicatorView.startAnimating()
-        } else {
-            indicatorView.isHidden = true
-            indicatorView.stopAnimating()
+        DispatchQueue.main.async { [weak self] in
+            self?.cellView.imageView.image = UIImage.gifImageWithURL(gifURL)
+            self?.cellView.indicatorAction(bool: false)
         }
     }
 }
