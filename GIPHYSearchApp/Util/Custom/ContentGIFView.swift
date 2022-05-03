7153834abb24f7da26275796d2a01f174de15f42
EthanPark <ethanpark604@gmail.com>
2022-05-03 18:15:20 +0900

    [Add] ContentGIFView

---
+++ GIPHYSearchApp/Util/Custom/ContentGIFView.swift
@@ -0,0 +1,66 @@
+//
+//  ContentGIFView.swift
+//  GIPHYSearchApp
+//
+//  Created by 박근보 on 2022/05/03.
+//
+
+import UIKit
+
+final class ContentGIFView: BaseView {
+
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .black
+        imageView.layer.masksToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    let indicatorView: UIActivityIndicatorView = {
+        let indicatorView = UIActivityIndicatorView()
+        indicatorView.hidesWhenStopped = true
+        indicatorView.color = .white
+        indicatorView.backgroundColor = .black
+        indicatorView.translatesAutoresizingMaskIntoConstraints = false
+        return indicatorView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpView()
+        setUpConstraint()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func setUpView() {
+
+        addSubview(imageView)
+        addSubview(indicatorView)
+    }
+
+    override func setUpConstraint() {
+
+        imageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+
+        indicatorView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        indicatorView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        indicatorView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        indicatorView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+
+    func indicatorAction(bool: Bool) {
+        if bool {
+            indicatorView.isHidden = false
+            indicatorView.startAnimating()
+        } else {
+            indicatorView.isHidden = true
+            indicatorView.stopAnimating()
+        }
+    }
+}
--- GIPHYSearchApp/Util/Custom/ContentImageView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ContentImageView.swift
-//  GIPHYSearchApp
-//
-//  Created by 박근보 on 2022/05/03.
-//
-
-import Foundation
