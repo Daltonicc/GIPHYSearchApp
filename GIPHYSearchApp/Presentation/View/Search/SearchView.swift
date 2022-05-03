caa3633c81083822d9d65867f95f265d938a3678
EthanPark <ethanpark604@gmail.com>
2022-05-03 11:08:00 +0900

    [Edit] CollectionView top sectionInset

--- GIPHYSearchApp/Presentation/View/Search/SearchView.swift
+++ GIPHYSearchApp/Presentation/View/Search/SearchView.swift
@@ -25,7 +25,7 @@ final class SearchView: BaseView {
         layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 2 - 2, height: UIScreen.main.bounds.width / 3 - 20)
         layout.minimumLineSpacing = 2
         layout.minimumInteritemSpacing = 2
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 0)
         layout.scrollDirection = .vertical
         collectionView.backgroundColor = .black
         collectionView.translatesAutoresizingMaskIntoConstraints = false
