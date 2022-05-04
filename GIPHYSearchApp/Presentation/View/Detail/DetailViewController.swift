f5919875ad559c54a006928236b8602274adad9b
EthanPark <ethanpark604@gmail.com>
2022-05-04 13:22:15 +0900

    [Edit] DetailViewController Config

--- GIPHYSearchApp/Presentation/View/Detail/DetailViewController.swift
+++ GIPHYSearchApp/Presentation/View/Detail/DetailViewController.swift
@@ -13,6 +13,7 @@ final class DetailViewController: BaseViewController {
     var viewModel: DetailViewModel?
 
     var item: GIFItem?
+    var favoriteItem: GIFFavoriteItem?
     var isFavorite = false
 
     override func loadView() {
@@ -22,48 +23,69 @@ final class DetailViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
-        contentViewConfig()
+        contentViewConfigBySearch()
+        contentViewConfigByFavorite()
     }
 
     override func setViewConfig() {
-
         mainView.favoriteButton.addTarget(self, action: #selector(favoriteButtonClicked), for: .touchUpInside)
     }
 
     override func navigationItemConfig() {
-        
         navigationItem.leftBarButtonItem = backBarButton
     }
 
-    private func contentViewConfig() {
-        guard let item = item else { return }
-        guard let viewModel = viewModel else { return }
+    private func contentViewConfig(avatar: String, gif: String, name: String, height: String, isFavorite: Bool) {
+        let height: Int32 = Int32(height) ?? 0
 
-        let height: Int32 = Int32(item.images.original.height) ?? 0
-        isFavorite = viewModel.checkDatabase(item: item)
         mainView.contentView.indicatorAction(bool: true)
-        mainView.userImageView.setImageUrl(item.user.avatarURL)
-        mainView.contentView.heightAnchor.constraint(equalToConstant: CGFloat(height)).isActive = true
-        mainView.usernameLabel.text = item.user.name
+        mainView.userImageView.setImageUrl(avatar)
+        mainView.usernameLabel.text = name
         mainView.favoriteButton.tintColor = isFavorite ? .systemRed : .white
-        let gifURL = item.images.original.url
+        mainView.contentView.heightAnchor.constraint(equalToConstant: CGFloat(height)).isActive = true
+
         DispatchQueue.main.async { [weak self] in
-            self?.mainView.contentView.imageView.image = UIImage.gifImageWithURL(gifURL)
+            self?.mainView.contentView.imageView.image = UIImage.gifImageWithURL(gif)
             self?.mainView.contentView.indicatorAction(bool: false)
         }
+    }
+
+    private func contentViewConfigBySearch() {
+        guard let item = item else { return }
+        guard let viewModel = viewModel else { return }
+        isFavorite = viewModel.checkDatabase(item: item)
+        contentViewConfig(avatar: item.user.avatarURL,
+                          gif: item.images.original.url,
+                          name: item.user.name,
+                          height: item.images.original.height,
+                          isFavorite: isFavorite)
+    }
+
+    private func contentViewConfigByFavorite() {
+        guard let favoriteItem = favoriteItem else { return }
+        isFavorite = true
+        contentViewConfig(avatar: favoriteItem.avatarURL ?? "",
+                          gif: favoriteItem.originalURL ?? "",
+                          name: favoriteItem.username ?? "",
+                          height: favoriteItem.originalHeight ?? "",
+                          isFavorite: favoriteItem.isFavorite)
+    }
+
+    private func removeAlert() {
+
     }
 
     @objc private func favoriteButtonClicked() {
         addPressAnimationToButton(scale: 0.85, mainView.favoriteButton) { [weak self] _ in
             guard let self = self else { return }
-            guard let item = self.item else { return }
             guard let viewModel = self.viewModel else { return }
 
             self.isFavorite.toggle()
             self.mainView.favoriteButton.tintColor = self.isFavorite ? .systemRed : .white
-            viewModel.pressFavoriteButton(item: item)
+            viewModel.pressFavoriteButton(item: self.item, favoriteItem: self.favoriteItem) {
+                self.removeAlert()
+            }
         }
-
     }
 }
 
