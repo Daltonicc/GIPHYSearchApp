21d62c3e6aea58e60852e64b91b13644f0d8b3de
EthanPark <ethanpark604@gmail.com>
2022-05-04 13:24:11 +0900

    [Add] FavoriteViewModel

--- GIPHYSearchApp/Presentation/ViewModel/FavoriteViewModel.swift
+++ GIPHYSearchApp/Presentation/ViewModel/FavoriteViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+protocol FavoriteViewModelProtocol {
+    func fetchFavoriteGIFItemList()
+}
+
+final class FavoriteViewModel: FavoriteViewModelProtocol {
+
+    var gifFavoriteItemList: Observable<[GIFFavoriteItem]> = Observable([])
+
+    func fetchFavoriteGIFItemList() {
+        gifFavoriteItemList.value = CoreDataManager.shared.fetchData(request: GIFFavoriteItem.fetchRequest())
+    }
+}
